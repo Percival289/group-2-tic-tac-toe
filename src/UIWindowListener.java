62f076cc433b34002e2ebb564155b5307d9645e5
Percival289 <57201358+Percival289@users.noreply.github.com>
2020-11-05 15:55:22 -0500

    Delete UIWindowListener.java

--- src/UIWindowListener.java
+++
@@ -1,8 +0,0 @@
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-public class UIWindowListener extends WindowAdapter {
-    public void windowClosing(WindowEvent windowEvent) {
-       System.exit(0);
-    }
-}
